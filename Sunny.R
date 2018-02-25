77c7cf6c32d77659dd96bea7dbb5cb6afde5d601
1sunnyhong <36501151+1sunnyhong@users.noreply.github.com>
2018-02-25 17:47:34 -0500

    Add files via upload

---
+++ Sunny.R
@@ -0,0 +1,12 @@
+# What is the data problem? What is the final managerial objective?
+
+# Describe the measurement types of each variable
+action_taken: Nominal 
+action_taken_name: 
+
+# Create a table summarizing the range or variation in each variable.
+## Add statistics (mean,median, standard deviation, etc.) as you deem necessary.
+# How do you handle missing data in this dataset?
+# Provide histograms/density plots for key variables, such as action taken on loan, gender,ethnicity, etc.
+# Create bivariate frequency distributions (tables or plots) for key variables
+# Discuss what the data patterns indicate, and what this could mean for the probability of getting a loan approved.
